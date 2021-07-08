7ee653603d7f1e0c556143e1e8630dc4f4d64fef
unknown <isidoreducasse79@gmail.com>
2021-07-08 23:08:50 +0200

    product details component

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/model/product';
+import { ShopParams } from 'src/app/shared/model/shop-params';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -10,12 +11,32 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  products: IProduct[];
+  shopParams = new ShopParams();
+  selection: IProduct[] = [];
 
   constructor(private shopService: ShopService, private routeActive: ActivatedRoute) { }
-  
+
   ngOnInit(): void {
+    this.shopParams.pageSize = 50;
     this.getProductById();
+    this.getProducts();
+    // this.getRandomProds();
   }
+
   getProductById = () => this.shopService.getProductById(Number(this.routeActive.snapshot.paramMap.get('id')))
     .subscribe(p => this.product = p, err => console.log(err));
+
+  getProducts = () => this.shopService.getProducts(this.shopParams)
+    .subscribe(p => this.products = p.data, err => console.log(err));
+
+  //  getRandomProds = async () => {
+  //   var prods = await this.shopService.getProducts(this.shopParams).toPromise();
+  //   for (let index = 0; index < 4; index++) {
+  //     let random = prods.data[Math.floor(Math.random()*prods.data.length)].id;
+  //     const element: IProduct = prods.data[random];
+  //     this.selection.push(element);
+  //   }
+  //   return this.selection;
+  // }
 }
