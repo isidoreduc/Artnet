cbaa08fb9bdc6d7975c8caff3a080687e280eb34
isidoreducasse <isidoreducasse79@gmail.com>
2021-07-17 16:56:08 +0200

    server: basket validation - price, quantity etc.

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.DTOs;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,22 +10,24 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
 
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasketAsync(string basketId) => 
+        public async Task<ActionResult<BasketDto>> GetBasketAsync(string basketId) =>
             Ok(await _basketRepository.GetBasketAsync(basketId) ?? new Basket(basketId)); // if the client returns after 30 days, his basket expired, so we create a new, empty one with same id
 
         [HttpPost]
-        public async Task<ActionResult<Basket>> CreateOrUpdateBasketAsync(Basket basket) => 
-            Ok(await _basketRepository.CreateOrUpdateBasketAsync(basket));
+        public async Task<ActionResult<Basket>> CreateOrUpdateBasketAsync(BasketDto basketDto) =>
+            Ok(await _basketRepository.CreateOrUpdateBasketAsync(_mapper.Map<BasketDto, Basket>(basketDto)));
 
         [HttpDelete]
-        public async Task<bool> DeleteBasketAsync(string basketId) => 
+        public async Task<bool> DeleteBasketAsync(string basketId) =>
             await _basketRepository.DeleteBasketAsync(basketId);
     }
 }
--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -23,6 +23,8 @@ namespace API.Helpers
                 .ForMember(destinationMember => destinationMember.PictureUrl, memberOptions => memberOptions.MapFrom<ProductImageUrlResolver>());
 
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Basket, BasketDto>().ReverseMap();
+            CreateMap<BasketItem, BasketItemDto>().ReverseMap();
 
         }
     }
