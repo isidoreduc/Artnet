e3a21ba029d9e3a7fb079670e3374e49420ce74a
isidoreducasse <isidoreducasse79@gmail.com>
2021-07-17 15:33:09 +0200

    server: address data validation

--- API/DTOs/AddressDto.cs
+++ API/DTOs/AddressDto.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class AddressDto
+    // always validate at DTO level, not entity level
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public string ZipCode { get; set; }
     }
 }
