ea6211ac6bb97d8b73fa79f7b32f6e1cd8c815cd
isidoreducasse <isidoreducasse79@gmail.com>
2021-07-27 12:51:35 +0200

    client: saving/updating the user address in the address form

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 import { AccountService } from 'src/app/account/account.service';
 import { IUserAddress } from 'src/app/shared/model/user';
 
@@ -10,8 +13,9 @@ import { IUserAddress } from 'src/app/shared/model/user';
 })
 export class CheckoutAddressComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
+  navigationSubscription: Subscription;
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastrService: ToastrService) {}
 
   ngOnInit(): void {
     this.getUserAddress();
@@ -20,9 +24,22 @@ export class CheckoutAddressComponent implements OnInit {
 
   getUserAddress = () => this.accountService.getUserAddress().subscribe(
     (a: IUserAddress) => {
-      if(a) this.checkoutForm.get("addressForm").patchValue(a);
+      if (a) this.checkoutForm.get("addressForm").patchValue(a);
     }, err => console.log(err));
 
-  updateUserAddress = () => this.accountService.updateUserAddress(this.checkoutForm.get("addressForm").value);
+  updateUserAddress = () => this.accountService.updateUserAddress(this.checkoutForm.get("addressForm").value)
+    .subscribe(
+      () => {
+        this.toastrService.success("User address successfully updated", "Update success");
+      },
+      err => {
+        console.log(err);
+        this.toastrService.error(err.message, "Update error");
+      }
+    );
+
+
+
+
 
 }
