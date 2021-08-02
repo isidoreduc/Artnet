c35441e5e5b98683b68ad13b442e0bdee01a2f8d
isidoreducasse <isidoreducasse79@gmail.com>
2021-08-02 18:03:48 +0200

    client: showing a spinner on payment button while busy

--- client/src/app/core/interceptors/spinner.interceptor.ts
+++ client/src/app/core/interceptors/spinner.interceptor.ts
@@ -15,10 +15,15 @@ export class SpinnerInterceptor implements HttpInterceptor {
   constructor(private spinnerService: SpinnerService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // put spinner while creating order
+    if(request.method === "POST" && request.url.includes("api/order")){
+      return next.handle(request);
+    }
+    // put spinner while checking for email exists at registering
     if(!request.url.includes("emailexists"))
       this.spinnerService.busyServer();
     return next.handle(request).pipe(
-      delay(300),
+      // delay(300),
       finalize(() => this.spinnerService.idleServer())
     );
   }
